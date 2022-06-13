ce379a3e79f28dfbf1aba598d620501f96e6440c
Tomi Prasetyo <tomiprasetyo95@gmail.com>
2022-06-13 10:44:13 +0700

    make flag and switch case

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/tomiprasetyo/golang-cli-todo-app/todo"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("Hello")
+	// make flag
+	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
+
+	// unite
+	todos := &todo.Todos{}
+
+	// load todo when start the application
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	// switch case
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
+
 }
