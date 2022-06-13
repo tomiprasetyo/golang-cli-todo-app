cb3972272c5f31bc6a3d8e2652165f428a6c2605
Tomi Prasetyo <tomiprasetyo95@gmail.com>
2022-06-13 10:14:32 +0700

    add method delete and method load file

--- todo/todo.go
+++ todo/todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -32,16 +35,58 @@ func (t *Todos) Add(task string) {
 // method completed
 func (t *Todos) Complete(index int) error {
 	// get reference to the main data structure aka Todos
-	listTodos := *t
+	listOfTodos := *t
 
 	// check length of the index
-	if index <= 0 || index > len(listTodos) {
+	if index <= 0 || index > len(listOfTodos) {
 		return errors.New("invalid index")
 	}
 
 	// if valid
-	listTodos[index-1].CompletedAt = time.Now()
-	listTodos[index-1].Done = true
+	listOfTodos[index-1].CompletedAt = time.Now()
+	listOfTodos[index-1].Done = true
+
+	return nil
+}
+
+// method delete
+func (t *Todos) Delete(index int) error {
+	// get reference to the main data structure aka Todos
+	listOfTodos := *t
+
+	// check length of the index
+	if index <= 0 || index > len(listOfTodos) {
+		return errors.New("invalid index")
+	}
+
+	// dereference
+	*t = append(listOfTodos[:index-1], listOfTodos[index:]...)
+
+	return nil
+}
+
+// method load
+func (t *Todos) Load(filename string) error {
+	// read file
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// if file not found
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	// check the length of the file
+	if len(file) == 0 {
+		return err
+	}
+
+	// convert data from file to the struct object (unmarshal the data)
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
