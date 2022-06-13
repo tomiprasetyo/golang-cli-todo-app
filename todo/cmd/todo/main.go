18f4b7d5e0ae52d5345cc0ff97b9fff6ce1c9c85
Tomi Prasetyo <tomiprasetyo95@gmail.com>
2022-06-13 10:54:46 +0700

    add flag complete

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -15,6 +15,7 @@ const (
 func main() {
 	// make flag
 	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
 
 	flag.Parse()
 
@@ -32,6 +33,19 @@ func main() {
 	case *add:
 		todos.Add("Sample todo")
 		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(todoFile)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
